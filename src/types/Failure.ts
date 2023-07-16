381ba340093dbbb87fc997176657d90649cc1dab
Michael Hubbard <michaelh@laine.co.za>
2023-07-16 12:59:16 +0200

    Local test mode and basic failure tracking

--- src/types/Failure.ts
+++ src/types/Failure.ts
@@ -2,9 +2,10 @@ import { Metric } from "./Metric"
 
 export type Failure = {
     start: Date,
+    last_observed: Date,
     end: Date|null,
-    type: Metric,
-    value: string,
+    type: Metric|string,
+    message: string,
     count: number,
     notified: boolean,
     resolved: boolean
--- src/watcher.ts
+++ src/watcher.ts
@@ -3,6 +3,7 @@ import { Notifier } from './notifier'
 import { Failure } from './types/Failure'
 import { WatcherConstructorParams } from './types/WatcherConstructorParams'
 import * as log4js from 'log4js'
+import * as fs from 'fs'
 import { WatchMetrics } from './types/WatchMetrics'
 import { Metric } from './types/Metric'
 
@@ -13,6 +14,7 @@ export class Watcher {
     private metrics: WatchMetrics[]|null = null
     private target: string|null = null
     private logger: log4js.Logger
+    private test_counter: number = 0
 
     public constructor(params: WatcherConstructorParams) {
         this.target = params.target
@@ -27,9 +29,10 @@ export class Watcher {
 
         this.failures.push({
             start: new Date(),
+            last_observed: new Date(),
             end: null,
             type: Metric.current_round,
-            value: '',
+            message: '',
             count: 0,
             notified: false,
             resolved: false
@@ -43,24 +46,68 @@ export class Watcher {
     }
 
     private async fetch_metrics(): Promise<void> {
-        if(this.target !== null) {
+        if(process.env.TEST_METRICS != undefined && !JSON.parse(process.env.TEST_METRICS)) {
+            if(this.target !== null) {
 
-            this.logger.info('Fetching metrics from target: '+this.target)
+                this.logger.info('Fetching metrics from target: '+this.target)
+    
+                axios.get(this.target)
+                    .then(response => {
+                        if(response.data != '') {
+                            this.metrics = this.parse_metrics(response.data)
+                            this.logger.trace(this.metrics)
+                        }
+                    })
+                    .then(() => {
+                        this.repeat_fetch_metrics()
+                    })
+                    .catch(error => {
+                        let error_value = 'Unknown'
+                        
+                        if (error.response) {
+                            error_value = error.reponse.status+' '+error.response.data
+                            
+                            } else if (error.request) {
+                            error_value = error.request
+                          } else {
+                            error_value = error.message
+                          }
+                        this.logger.error('Unable to fetch metrics. Error: '+error_value)
+                        this.process_failure({
+                            start: new Date(),
+                            last_observed: new Date(),
+                            end: null,
+                            type: 'MetricsCollectionError',
+                            message: error_value,
+                            count: 0,
+                            notified: false,
+                            resolved: false
+                        })
 
-            axios.get(this.target)
-                .then(response => {
-                    if(response.data != '') {
-                        this.metrics = this.parse_metrics(response.data)
-                        this.logger.trace(this.metrics)
+                        this.repeat_fetch_metrics()
+                    })
+            }
+            else {
+                this.logger.error('Target is null, unable to fetch metrics.')
+            }
+        }
+        else {
+            if(process.env.TEST_METRICS_FILENAME != undefined) {
+                await fs.readFile(process.env.TEST_METRICS_FILENAME, 'utf-8', (err, data) => {
+                    if(err) {
+                        this.logger.error('Unable to open test metrics file: '+err.message)
+                        return
                     }
+                    this.metrics = this.parse_metrics(data,true)
+                    this.logger.trace(this.metrics)
                 })
-                .then(() => {
-                    this.repeat_fetch_metrics()
-                })
-                .catch(error => {
-                    this.logger.error('Unable to fetch metrics. Error: '+error)
-                    this.repeat_fetch_metrics()
-                })
+                
+                this.repeat_fetch_metrics()
+                
+            }
+            else {
+                this.logger.error('TEST_METRICS_FILENAME is not set')
+            }
         }
         
     }
@@ -69,10 +116,16 @@ export class Watcher {
         setTimeout(() => this.fetch_metrics(), (process.env.METRICS_REFRESH_INTERVAL != undefined) ? parseInt(process.env.METRICS_REFRESH_INTERVAL) : 5000)
     }
 
-    private parse_metrics(metrics: string): WatchMetrics[] {
+    private parse_metrics(metrics: string, is_test: boolean = false): WatchMetrics[] {
         let parsed: WatchMetrics[] = []
 
         let split = metrics.split(/\r?\n/)
+
+        let test_increment = (is_test && process.env.TEST_FAIL != undefined && !JSON.parse(process.env.TEST_FAIL))
+            ?   this.test_counter
+            : 0
+        this.test_counter++
+        this.logger.trace('Test increment value is '+test_increment)
 
         split.every(line => {
             if(parsed.length == Object.values(Metric).length) return false;
@@ -82,12 +135,35 @@ export class Watcher {
             if(Object.values(Metric).includes(metric[0])) {
                 parsed.push({
                     name: metric[0],
-                    value: parseInt(metric[1])
+                    value: parseInt(metric[1]) + test_increment
                 })
             }
             return true
         })
 
         return parsed
+    }
+
+    private process_failure(failure: Failure): void {
+        let match = this.failures.find(item => {
+            if(!item.resolved && item.type === failure.type && item.message == failure.message) return true
+            return false
+        })
+        if(match != undefined) {
+            let index = this.failures.indexOf(match)
+            this.failures[index] = {
+                start: match.start,
+                last_observed: new Date(),
+                end: null,
+                type: match.type,
+                message: match.message,
+                count: match.count++,
+                notified: match.notified,
+                resolved: match.resolved
+            }
+        }
+        else {
+            this.failures.push(failure)
+        }
     }
 }
